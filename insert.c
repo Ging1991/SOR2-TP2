e8a6a1cfcd3210ce34d814cee4ac85914670e1ad
PabloIgeNakagawa <pabloigenaka@gmail.com>
2025-04-16 17:52:32 -0300

    codigo base

---
+++ insert.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+ 
+void insert_substring(char*, char*, int);
+char* substring(char*, int, int);
+ 
+int main()
+{
+   char text[100], substring[100];
+   int position;
+ 
+   printf("Enter some text\n");
+   fgets(text, sizeof(text), stdin);
+   text[strcspn(text, "\n")] = '\0'; // Elimina el salto de línea si existe
+ 
+   printf("Enter the string to insert\n");
+   fgets(substring, sizeof(substring), stdin);
+   substring[strcspn(substring, "\n")] = '\0'; // Elimina el salto de línea si existe
+ 
+   printf("Enter the position to insert\n");
+   scanf("%d", &position);
+
+   // Validación de la posición
+   if (position < 1 || position > strlen(text)) {
+      printf("Error: Posición inválida. Debe estar entre 1 y %ld.\n", strlen(text));
+      return 1;  // Termina el programa con error
+   }
+ 
+   insert_substring(text, substring, position);
+ 
+   printf("%s\n",text);
+ 
+   return 0;
+}
+ 
+void insert_substring(char *a, char *b, int position)
+{
+   char *f, *e;
+   int length;
+ 
+   length = strlen(a);
+ 
+   f = substring(a, 1, position - 1 );      
+   e = substring(a, position, length-position+1);
+ 
+   strcpy(a, "");
+   strcat(a, f);
+   free(f);
+   strcat(a, b);
+   strcat(a, e);
+   free(e);
+}
+ 
+char *substring(char *string, int position, int length) 
+{
+   char *pointer;
+   int c;
+ 
+   pointer = malloc(length+1);
+ 
+   if( pointer == NULL )
+       exit(EXIT_FAILURE);
+ 
+   for( c = 0 ; c < length ; c++ ) 
+      *(pointer+c) = *((string+position-1)+c);       
+ 
+   *(pointer+c) = '\0';
+ 
+   return pointer;
+}
